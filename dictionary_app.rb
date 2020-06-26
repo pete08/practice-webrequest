4ecbbb21b8413d4ab38c3b3d3a7d9663be71bcd2
PeterH <pete08@gmail.com>
2020-06-25 20:20:18 -0500

    Add definiton and pronounciation to API wordnik app

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -6,11 +6,14 @@ p "Want to know the definiton of your word?"
 word_input = gets.chomp
 
 defrequest = HTTP.get("https://api.wordnik.com/v4/word.json/#{word_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{apikey}")
+pronouncerequest = HTTP.get("https://api.wordnik.com/v4/word.json/#{word_input}/pronunciations?useCanonical=false&limit=50&api_key=#{apikey}")
 
 definition = defrequest.parse[0]["text"]
 word = defrequest.parse[0]["word"]
+pronounce = pronouncerequest.parse[0]["raw"]
 
 p "The definition of #{word} is: #{definition}"
+p "#{word} is pronounced: #{pronounce}"
 
 
 # [0]["text"]
