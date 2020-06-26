5c10829f153048079265bc15d62623c7d95ab515
PeterH <pete08@gmail.com>
2020-06-25 21:02:59 -0500

    Add loop to dictionary for unlimited definitions

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,19 +1,26 @@
 require "http"
 
 apikey = ''
-p "Want to know the definiton of your word?"
+# word_input = ""
 
-word_input = gets.chomp
-
-defrequest = HTTP.get("https://api.wordnik.com/v4/word.json/#{word_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{apikey}")
-pronouncerequest = HTTP.get("https://api.wordnik.com/v4/word.json/#{word_input}/pronunciations?useCanonical=false&limit=50&api_key=#{apikey}")
-
-definition = defrequest.parse[0]["text"]
-word = defrequest.parse[0]["word"]
-pronounce = pronouncerequest.parse[0]["raw"]
-
-p "The definition of #{word} is: #{definition}"
-p "#{word} is pronounced: #{pronounce}"
-
+while true
+  puts "Want to know the definiton of your word?"
+  puts "type \"exit now\" to exit"
+  word_input = gets.chomp
+  if  word_input != "exit now"
+    defrequest = HTTP.get("https://api.wordnik.com/v4/word.json/#{word_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{apikey}")
+    pronouncerequest = HTTP.get("https://api.wordnik.com/v4/word.json/#{word_input}/pronunciations?useCanonical=false&limit=50&api_key=#{apikey}")
+    
+    definition = defrequest.parse[0]["text"]
+    word = defrequest.parse[0]["word"]
+    pronounce = pronouncerequest.parse[0]["raw"]
+    
+    p "The definition of #{word} is: #{definition}"
+    p "#{word} is pronounced: #{pronounce}"
+  else
+    break
+  end
+  puts ""
+end
 
 # [0]["text"]
